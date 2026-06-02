42e2230678fde12b6103c779a4edc85f6ffa761e
Eunchae <asdfg041231@naver.com>
2026-06-02 22:31:53 +0900

    Fix email error logging

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,12 +1,24 @@
 const transporter = require("../config/mailer");
 
 async function sendEmail({ from, to, subject, html }) {
-  return transporter.sendMail({
-    from,
-    to,
-    subject,
-    html,
-  });
+  try {
+    return await transporter.sendMail({
+      from,
+      to,
+      subject,
+      html,
+    });
+  } catch (error) {
+    console.error("[EMAIL_SEND_FAILED]", {
+      code: error.code,
+      command: error.command,
+      responseCode: error.responseCode,
+      response: error.response,
+      message: error.message,
+    });
+
+    throw error;
+  }
 }
 
 module.exports = {
